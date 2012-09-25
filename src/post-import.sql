f4833bb75f44ed84fd9a969f26a4bb89813d5edd
Dan Poltawski <talktodan@gmail.com>
2012-09-26 07:33:50 +0800

    Add post-import script

---
+++ src/post-import.sql
@@ -0,0 +1,13 @@
+create table danp_computed_stops (
+  stop_id text , --REFERENCES gtfs_stops(stop_id),
+  route_type  int
+);
+
+INSERT INTO danp_computed_stops SELECT DISTINCT(stop_id), '2' AS route_type FROM gtfs_stop_times WHERE trip_id IN 
+(SELECT trip_id FROM gtfs_trips tr JOIN gtfs_routes ro ON ro.route_id = tr.route_id WHERE ro.route_type = 2);
+
+INSERT INTO danp_computed_stops SELECT DISTINCT(stop_id), '3' AS route_type FROM gtfs_stop_times WHERE trip_id IN 
+(SELECT trip_id FROM gtfs_trips tr JOIN gtfs_routes ro ON ro.route_id = tr.route_id WHERE ro.route_type = 3);
+
+INSERT INTO danp_computed_stops SELECT DISTINCT(stop_id), '4' AS route_type FROM gtfs_stop_times WHERE trip_id IN 
+(SELECT trip_id FROM gtfs_trips tr JOIN gtfs_routes ro ON ro.route_id = tr.route_id WHERE ro.route_type = 4);
