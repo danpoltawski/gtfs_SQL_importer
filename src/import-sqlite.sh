7633da77abece2ac23487c39ed6fe7128ce1aec7
Dan Poltawski <talktodan@gmail.com>
2012-11-03 16:36:14 +0800

    Add computed stops

--- src/import-sqlite.sh
+++ src/import-sqlite.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 cat gtfs_tables.sqlite <(python import_gtfs_to_sql.py feed nocopy) | sqlite3 transit.db
+cat post-import.sql | sqlite3 transit.db
--- src/post-import.sql
+++ src/post-import.sql
@@ -11,3 +11,5 @@ INSERT INTO danp_computed_stops SELECT DISTINCT(stop_id), '3' AS route_type FROM
 
 INSERT INTO danp_computed_stops SELECT DISTINCT(stop_id), '4' AS route_type FROM gtfs_stop_times WHERE trip_id IN 
 (SELECT trip_id FROM gtfs_trips tr JOIN gtfs_routes ro ON ro.route_id = tr.route_id WHERE ro.route_type = 4);
+
+CREATE INDEX danp_route_type_idx ON danp_computed_stops (stop_id, route_type);
