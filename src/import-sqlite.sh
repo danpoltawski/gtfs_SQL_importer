0896bb8e27422f6c72d9c3f046dc481dd7b2178c
Dan Poltawski <talktodan@gmail.com>
2012-11-11 11:11:01 +0800

    Do whole download and create db

--- src/import-sqlite.sh
+++ src/import-sqlite.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+set -e
 
+rm transit.db
+rm  google_transit.zip
+rm -r feed
+curl -O http://www.transperth.wa.gov.au/TimetablePDFs/GoogleTransit/google_transit.zip
+mkdir feed
+cd feed
+unzip ../google_transit.zip
+cd ..
 cat gtfs_tables.sqlite <(python import_gtfs_to_sql.py feed nocopy) | sqlite3 transit.db
 cat post-import.sql | sqlite3 transit.db
+echo 'VACUUM;' | sqlite3 transit.db
