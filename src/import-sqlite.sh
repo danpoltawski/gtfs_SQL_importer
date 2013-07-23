984ac122068e0edb3bfa18f24a3642c60fff0b55
Dan Poltawski <talktodan@gmail.com>
2013-07-23 21:36:01 +0800

    Add optimised db thingy

--- src/import-sqlite.sh
+++ src/import-sqlite.sh
@@ -5,13 +5,13 @@ timestamp=`date +%Y%m%d%H%M`
 timestampfilename=`date +%Y-%m-%d-%H%M`
 filename=transit-${timestamp}.db
 
-[ -f google_transit.zip ] && rm  google_transit.zip
-[ -d feed ] && rm -r feed
-curl -O http://www.transperth.wa.gov.au/TimetablePDFs/GoogleTransit/google_transit.zip
-mkdir feed
-cd feed
-unzip ../google_transit.zip
-cd ..
+#[ -f google_transit.zip ] && rm  google_transit.zip
+#[ -d feed ] && rm -r feed
+#curl -O http://www.transperth.wa.gov.au/TimetablePDFs/GoogleTransit/google_transit.zip
+#mkdir feed
+#cd feed
+#unzip ../google_transit.zip
+#cd ..
 cat gtfs_tables.sqlite <(python import_gtfs_to_sql.py feed nocopy) | sqlite3 $filename
 cat post-import.sql | sqlite3 $filename
 echo 'CREATE TABLE shouldirun_config (name, value);' | sqlite3 $filename
